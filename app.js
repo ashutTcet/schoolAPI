0ba0810bbb298ff65d4a542490450434bc293119
ashutTcet <1032240757@tcetmumbai.in>
2026-05-06 00:49:59 +0530

    fixed url and routing error

--- app.js
+++ app.js
@@ -6,17 +6,14 @@ const schoolRoutes = require('./routes/schoolRoutes');
 
 const app = express();
 
-// Middleware
 app.use(cors());
-app.use(express.json()); // IMPORTANT (fixes req.body issue)
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.use('/', schoolRoutes);
 
-// Health check (optional but useful for deployment)
 app.get('/', (req, res) => {
-  res.send("🚀 School API is running...");
+  res.send("School API is running...");
 });
 
 // Port (Render uses process.env.PORT)
